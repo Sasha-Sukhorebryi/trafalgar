01d7a9fe5458cc9af5c3a3dde76727231fc8defc
Sasha-Sukhorebryi <85243818+Sasha-Sukhorebryi@users.noreply.github.com>
2023-09-26 13:20:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//---Menu
+
+const burger = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+const body = document.body
+
+if (burger && menu) {
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('_active');
+		menu.classList.toggle('_active');
+		body.classList.toggle('_lock');
+	})
+}
+
+//---Swiper
+const swiper = new Swiper('.swiper', {
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
